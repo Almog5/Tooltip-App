b26a652727900f6f106dd028f856eba74a263bb1
mm15-123 <rupigroup20@gmail.com>
2020-06-03 16:37:23 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { rendererTypeName } from '@angular/compiler';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tooltip-nipendo';
+  title = 'Tooltip-Assignment-Nipendo';
+  access: boolean = false;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  changePageStyle() {
+    console.log("hello from access")
+    this.access = !this.access
+    if (this.access == true) {
+      document.body.style.backgroundColor = "#000000bf"
+      document.body.style.color = "white"  
+    }
+    else {
+      document.body.style.backgroundColor = "white"
+      document.body.style.color = "black"
+      
+    }
+  }
 }
+
---
+++ src/app/tooltip.directive.ts
@@ -0,0 +1,126 @@
+import { Directive, Input, HostListener, ElementRef, Renderer2 } from "@angular/core";
+
+
+@Directive({
+    selector: '[tooltip]',
+})
+export class TooltipDirective {
+    //@Input('tooltip') tooltipTitle: string;
+    @Input() placement: string;
+    @Input() delay: number;
+    clicked: boolean = false;
+    tooltip: HTMLElement;
+    offset = 10;
+    
+    constructor(private elRef: ElementRef, private renderer: Renderer2) {
+
+    }
+
+
+    @HostListener('click') onclick(eventData: Event) {
+        this.clicked = true;
+        console.log("hello from tooltip directive");
+        if (!this.tooltip) { this.show(); }
+
+    }
+
+    show() {
+        console.log("helloe from show()")
+        this.create();
+        this.setPosition();
+        this.renderer.addClass(this.tooltip, 'ng-tooltip-show');
+        this.onScroll(event) // on click check if tooltip is already out of range,
+                             // if thescreen is alredy scrolled down
+    }
+
+    hide() {
+        if (this.clicked == true) {
+            this.renderer.removeClass(this.tooltip, 'ng-tooltip-show');
+            window.setTimeout(() => {
+                this.renderer.removeChild(this.elRef.nativeElement, this.tooltip);
+                this.tooltip = null;
+            }, this.delay);
+            this.clicked = false;
+        }
+
+    }
+
+    create() {
+        console.log("hello from create()")
+        this.tooltip = this.renderer.createElement('span');
+
+        this.renderer.appendChild(
+            this.tooltip,
+            this.renderer.createText("hey i am tooltip") // tooltip textNode
+        );
+        //this.renderer.appendChild(document.body, this.tooltip);
+        this.renderer.appendChild(this.elRef.nativeElement, this.tooltip);
+
+        this.renderer.addClass(this.tooltip, 'ng-tooltip');
+        this.renderer.addClass(this.tooltip, `ng-tooltip-${this.placement}`);
+        // delay
+        this.renderer.setStyle(this.tooltip, '-webkit-transition', `opacity ${this.delay}ms`);
+        this.renderer.setStyle(this.tooltip, '-moz-transition', `opacity ${this.delay}ms`);
+        this.renderer.setStyle(this.tooltip, '-o-transition', `opacity ${this.delay}ms`);
+        this.renderer.setStyle(this.tooltip, 'transition', `opacity ${this.delay}ms`);
+
+    }
+
+    setPosition() {
+        console.log("hello from setPosition()")
+        const hostPos = this.elRef.nativeElement.getBoundingClientRect();
+        const tooltipPos = this.tooltip.getBoundingClientRect();
+        // window scroll top
+        const scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        let top, left;
+
+        if (this.placement === 'top') {
+            top = hostPos.top - tooltipPos.height - this.offset;
+            left = hostPos.left + (hostPos.width - tooltipPos.width) / 2;
+        }
+
+        if (this.placement === 'bottom') {
+            top = hostPos.bottom + this.offset;
+            left = hostPos.left + (hostPos.width - tooltipPos.width) / 2;
+        }
+
+        this.renderer.setStyle(this.tooltip, 'top', `${top + scrollPos}px`);
+        this.renderer.setStyle(this.tooltip, 'left', `${left}px`);
+    }
+
+    @HostListener('document:click', ['$event'])
+    clickout(event) {
+        if (this.elRef.nativeElement.contains(event.target)) {
+            console.log("clicked inside");
+        } else {
+            console.log("clicked outside");
+            this.hide()
+        }
+    }
+
+    @HostListener('window:scroll', ['$event']) 
+    onScroll(event) {
+        console.log("from onscroll()")
+        if (this.clicked == true) {
+
+            if (this.tooltip.offsetTop <= window.pageYOffset) {
+                console.log("hey' you out of range !")
+                this.placement = 'bottom'
+                this.setPosition()
+            }
+            else {
+                this.placement = 'top'
+                this.setPosition()
+            }
+        }
+    }
+    @HostListener('document:keydown.escape', ['$event'])
+    onKeydownHandler(event: KeyboardEvent) {
+        if (this.clicked == true) {
+            this.hide()
+        }
+    }
+
+}
+
+
